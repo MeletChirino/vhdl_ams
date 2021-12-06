c6c11914484d02b26b9af5f632e9bb3db83ff921
Melet <melet.chirino@gmail.com>
2021-12-06 11:39:48 +0100

    [UPDATE] TP finished

--- Calc_ABS_vide.vhd
+++ Calc_ABS_vide.vhd
@@ -2,18 +2,21 @@ library ieee;
 use ieee.mechanical_systems.all;
 use ieee.fluidic_systems.all;
 use ieee.electrical_systems.all;
+use ieee.std_logic_1164.all;
 
 entity Calc_ABS is
 port(
 terminal veh : translational_velocity;
 terminal roue : rotational_velocity;
 terminal hydro : fluidic;
-terminal vcmde : electrical;
+terminal vcmde : electrical
 );
 end Calc_ABS;
 
 architecture simple of Calc_ABS is
 	signal clk	: std_logic := '0';
+	signal vcom     : real := 5.0;
+	signal g : real;
 	quantity vv across ii through vcmde;
 	quantity pin across din through hydro;
 	quantity vit across force through veh;
@@ -21,33 +24,39 @@ architecture simple of Calc_ABS is
 begin
 
 	clk <= not clk after 1 ms;
-	--not sure about next few lines
+	vv == vcom;  
+	break on vcom; 
 	din == 0.0;
 	force == 0.0;
 	tq == 0.0;
 
 process(clk)
 	--check regulation voltage is vv, if not change vv for the actual value
-	variable R	: real := 12; --rayon roue
 	variable S	: real; --coeff glisement
 	begin 
 		if (clk'event and clk = '1') then -- detect rising edge 
-			S == (veh - omega * R) / veh; --calculs coeff glissement
-			if (veh > 30) then --if trasnlational speed under 30 adjust abs
+			
+			if (vit > 8.3) then --if trasnlational speed under 30 adjust abs
+				S := (vit - omega * 0.275) / vit; --calculs coeff glissement
 				--conditions described in paper
 				if(S >= 0.5) then 
-					vv := vv - 0.5;
+					if vcom > 0.5 then 
+						vcom <= vcom - 0.5;
+						end if; 
 				elsif (S < 0.5) then
-					vv := vv + 0.1;
-					end if;
-			elsif(veh < 30 et veh >  0) then 
+					if vcom < 4.9 then 
+						vcom <= vcom + 0.1;
+						end if;
+					end if; 
+			elsif(vit < 8.3 and vit >  0.0) then 
 				--when speed under 30 reestablish adljust voltage
-				vv := 5; -- tension de commande (?)	
+				vcom <= 5.0; -- tension de commande (?)	
 				end if;
-			if (pin = 0) then
-				vv := 5;
+			if (pin = 0.0) then
+				vcom <= 5.0;
 				end if;
 			end if;
+			g<=S;
 		end process;
 
 end simple;
--- test.vhd
+++ test.vhd
@@ -165,7 +165,7 @@ signal COND :real := 0.0;
 begin 
 
 cond <= 180.0 after 100 ms;
-f==cond'ramp(0.36);
+f==cond'ramp(0.5);
 
 Auto: 	entity vehicule(one) generic map (m=>m_veh,cx=>0.3,S=>1.8,v_init=>28.0) 
     port map(roue=>t1);
@@ -201,7 +201,7 @@ begin
 
 volt == 4.0;
 cond <= 180.0 after 100 ms;
-f==cond'ramp(0.36);
+f==cond'ramp(0.5);
 
 Auto: 	entity vehicule(one) generic map (m=>m_veh,cx=>0.3,S=>1.8,v_init=>28.0) 
     port map(roue=>t1);
@@ -220,6 +220,91 @@ u_REG:  entity regulateur(one) generic map (P=>1.0e-4, tau=>10.0)
 
 end H; 
 
+--------------------------------------------------------------------------------------------------------------
+-- Test véhicule, roue, frein, MC et ramp version 2 plus regulateur plus ABS Calculator
+--------------------------------------------------------------------------------------------------------------
+
+architecture I of test is 
+
+terminal t1 : translational_velocity;
+terminal t2 : rotational_velocity;
+terminal t3 : fluidic;
+terminal t4 : fluidic; 
+terminal t5 : electrical; 
+quantity f :FORCE := 180.0;
+signal COND :real := 0.0;
+
+begin 
+
+cond <= 180.0 after 100 ms, 0.0 after 5000 ms;
+f==cond'ramp(0.5, 0.1);
+--f==cond;
+--break on cond;
+
+Auto: 	entity vehicule(one) generic map (m=>m_veh,cx=>0.3,S=>1.8,v_init=>27.8) 
+    port map(roue=>t1);
+
+u_Roue:  entity roue(A) generic map(route=>humide, m=>m_veh, rR=>0.275, IR=>0.4, mu0_D=>1.0, As=>0.01, mu0_W=>0.5, Vc=>27.8)
+    port map(veh=>t1, frein => t2);
+
+u_frein:  entity frein(one) generic map (coef_fric=>0.36, S=>1.0e-3, R=>0.12)
+    port map(MC=>t3, roue => t2);
+
+u_MC:  entity maitre_cylindre(one) generic map (S=>1.0e-4, coef_assistance=>10.0)
+    port map(hyd=>t4, force => f);
+
+u_REG:  entity regulateur(one) generic map (P=>1.0e-4, tau=>10.0)
+    port map(IN_P=>t4, OUT_P => t3, V => t5);
+
+u_ABS:  entity Calc_ABS(simple) port map(veh => t1, roue => t2, hydro => t4, vcmde => t5);
+
+end I; 
+
+--------------------------------------------------------------------------------------------------------------
+-- Test véhicule, roue, frein, MC et ramp version 2 plus regulateur plus ABS Calculator
+--------------------------------------------------------------------------------------------------------------
+
+architecture J of test is 
+
+terminal t1 : translational_velocity;
+terminal t2 : rotational_velocity;
+terminal t3 : fluidic;
+terminal t4 : fluidic; 
+terminal t5 : electrical; 
+quantity f :FORCE := 180.0;
+quantity motor through t2;
+signal COND :real := 0.0;
+signal torque : real := 0.0; 
+
+begin 
+
+cond <= 180.0 after 7100 ms, 0.0 after 12000 ms;
+f==cond'ramp(0.5, 0.1);
+	torque 		<= -900.0 after 0 ms,
+			-500.0 after 1000 ms,
+			-300.0 after 3000 ms,
+			0.0 after 7000 ms;
+motor == torque; 
+break on torque; 
+
+Auto: 	entity vehicule(one) generic map (m=>m_veh,cx=>0.3,S=>1.8,v_init=>0.028) 
+    port map(roue=>t1);
+
+u_Roue:  entity roue(A) generic map(route=>humide, m=>m_veh, rR=>0.275, IR=>0.4, mu0_D=>1.0, As=>0.01, mu0_W=>0.5, Vc=>27.8)
+    port map(veh=>t1, frein => t2);
+
+u_frein:  entity frein(one) generic map (coef_fric=>0.36, S=>1.0e-3, R=>0.12)
+    port map(MC=>t3, roue => t2);
+
+u_MC:  entity maitre_cylindre(one) generic map (S=>1.0e-4, coef_assistance=>10.0)
+    port map(hyd=>t4, force => f);
+
+u_REG:  entity regulateur(one) generic map (P=>1.0e-4, tau=>10.0)
+    port map(IN_P=>t4, OUT_P => t3, V => t5);
+
+u_ABS:  entity Calc_ABS(simple) port map(veh => t1, roue => t2, hydro => t4, vcmde => t5);
+
+end J; 
 
 
 
