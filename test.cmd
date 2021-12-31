82c6b2715f26087d0ddd4ddd5363796d6fb31921
Melet <melet.chirino@gmail.com>
2021-12-31 16:16:55 +0100

    [ADD] text cmd file

---
+++ test.cmd
@@ -0,0 +1,15 @@
+* /home/beroy/BE_VHDL_AMS_BEROY_CHIRINO/test.cmd input netlist
+* Creation Date : Fri Nov 19 09:39:06% CET 2021 
+*
+
+
+.tran 0.1s 10s
+* User minimum step definition
+.option hmin=1e-12
+* User integration method selection
+.option trap
+* User ELDO accuracy specification
+.option eps=5e-3
+* User ELDO accuracy specification
+.option tuning=standard
+.end
